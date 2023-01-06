0af25e21e8fcd6721b67d973820cdeb8d1f8ad4f
Luizeduardovae <duduvivas18@hotmail.com>
2023-01-06 06:13:11 -0300

    Fix/Header Finalizado

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import styles from './Header.module.css'
-
+import CTlogo from '../IMG/CTLOGO.svg'
 
 export function Header() {
     return(
         <header className={styles.header}>
-            <strong className={styles.title}>Kamikaze Feed</strong>
+            <img src={CTlogo}></img>
+            <strong>Kamikaze Feed</strong>
         </header>
         
     );
