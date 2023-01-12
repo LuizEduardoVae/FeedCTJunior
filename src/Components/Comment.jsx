26d5ab99da9f98f8e8b3ce6b52103a9a224e6c28
Luizeduardovae <duduvivas18@hotmail.com>
2023-01-12 15:50:17 -0300

    teste

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -4,10 +4,10 @@ import style from './Comment.module.css'
 
 export function Comment(){
     return(
-        <div className={style.Comment}>
+        <div className={style.comment}>
             <img src="https://media.licdn.com/dms/image/D4D03AQGOwGoPP4w5fQ/profile-displayphoto-shrink_400_400/0/1671835597632?e=1678320000&v=beta&t=79p9YC37460-8HmOwcJzETHbS960Y0Y-E3wQxQyWrEQ" />
             <div className={style.commentBox}>
-                <div className={style.CommentContent}>
+                <div className={style.commentContent}>
                     <header>
                         <div className={style.authorAndTime}>
 
